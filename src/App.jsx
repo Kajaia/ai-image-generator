5e98b607a0a4a515541bc5ece9bbf8b0556c2d95
Kajaia <lashakajaia31@gmail.com>
2025-07-20 21:05:53 +0400

    Added quality, size and style parameters for image generation

--- src/App.jsx
+++ src/App.jsx
@@ -51,11 +51,12 @@ export default function App() {
   return (
     <div className="container">
       <div className="row justify-content-center">
+        <h1 className="text-center mb-4">AI Image Generator</h1>
         <div className="col-12 col-md-10">
           <div className="card rounded-3">
             <div className="card-body">
               <div className="row g-3">
-                <div className="col-12 col-md-5 col-lg-4">
+                <div className="col-12 col-lg-5">
                   <form onSubmit={(e) => generate(e)}>
                     <div>
                       <h2 className="fs-6 text-center mb-3">
@@ -78,9 +79,89 @@ export default function App() {
                         {loading ? "Generating image..." : "Submit"}
                       </button>
                     </div>
+                    <div className="mt-5">
+                      <div className="my-3">
+                        <h3 className="fs-6">Choose a quality</h3>
+                        <div className="d-flex align-items-center justify-content-evenly gap-2 rounded-3 p-2 bg-dark-secondary">
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              quality === "standard" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setQuality("standard")}
+                          >
+                            Standard
+                          </button>
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              quality === "hd" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setQuality("hd")}
+                          >
+                            HD
+                          </button>
+                        </div>
+                      </div>
+                      <div className="my-3">
+                        <h3 className="fs-6">Choose a size</h3>
+                        <div className="d-flex align-items-center justify-content-evenly gap-2 rounded-3 p-2 bg-dark-secondary flex-wrap flex-md-nowrap">
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              size === "1024x1024" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setSize("1024x1024")}
+                          >
+                            1024x1024
+                          </button>
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              size === "1792x1024" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setSize("1792x1024")}
+                          >
+                            1792x1024
+                          </button>
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              size === "1024x1792" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setSize("1024x1792")}
+                          >
+                            1024x1792
+                          </button>
+                        </div>
+                      </div>
+                      <div className="my-3">
+                        <h3 className="fs-6">Choose a style</h3>
+                        <div className="d-flex align-items-center justify-content-evenly gap-2 rounded-3 p-2 bg-dark-secondary">
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              style === "vivid" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setStyle("vivid")}
+                          >
+                            Vivid
+                          </button>
+                          <button
+                            type="button"
+                            className={`btn w-100 ${
+                              style === "natural" ? "bg-danger" : ""
+                            } border-0`}
+                            onClick={() => setStyle("natural")}
+                          >
+                            Natural
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                   </form>
                 </div>
-                <div className="col-12 col-md-7 col-lg-8">
+                <div className="col-12 col-lg-7">
                   {imageData && imageData?.url && (
                     <div>
                       <img
@@ -103,9 +184,16 @@ export default function App() {
                   )}
                   {!imageData && !imageData?.url && (
                     <div
-                      className="w-100 bg-secondary rounded-3"
+                      className="w-100 bg-dark-secondary rounded-3 d-flex align-items-center justify-content-center"
                       style={{ height: 500 }}
-                    ></div>
+                    >
+                      <img
+                        height={125}
+                        width={125}
+                        src="/logo.png"
+                        alt="Logo"
+                      />
+                    </div>
                   )}
                 </div>
               </div>
